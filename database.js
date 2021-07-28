4f35761352dd093a544c62cc3df78ed38a4c872a
Pyrex <retojonatan@gmail.com>
2021-07-28 12:16:56 -0300

    add db connection

---
+++ database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+(async () => {
+  try {
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    };
+
+    const db = await mongoose.connect(process.env.MONGODB_URI, options);
+    console.log(
+      "Connected to DB: (",
+      db.connection.name,
+      ") at:",
+      db.connection.host
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
